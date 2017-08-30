d2ee3c2689980e14842ee56c6281195b1346d0aa
Jamie Saxon <jsaxon@uchicago.edu>
2017-08-30 17:51:54 -0500

    ex script

--- salaries.sh
+++ salaries.sh
@@ -1,14 +1,48 @@
 #!/bin/bash
 
-# SORT CHICAGO SALARIES
+#f=$1
+f="salaries.csv"
 
-# start the party!!
-echo "Top 10 Salaries in Chicago"
-cat salaries.csv | 
-    grep '\$' |               # keep only lines with dollar signs.
-    sed 's/\$//g' |           # remove the dollar signs.
-    sed "s/\"//g" |           # remove the quotes
-    sort -t, -k 5 -n -r |     # sorting is the best.
-    head -10 | column -s, -t  # clean it up
+echo "(1) TOP SALARIES IN THE CITY!!"
+# Remove dollar signs, sort the appropriate column, and show the top few.
+cat $f | head -1
+echo
 
+echo "(2) CITY EMPLOYEES"
+# Count the lines, but careful of headers....
+cat $f | head -1
+echo 
+
+echo "(3 and 4) :: FULL AND PART-TIME WORKERS"
+# Cut out the apppropriate column, sort it, and count the types.
+cat $f | head -1
+echo
+
+echo "(5 and 6) HIGHEST HOURLY WAGES"
+# Same approach as the first question.
+cat $f | head -1
+echo 
+
+echo "(7) POLICE DEPARTMENT"
+# Just grep out the department and count the lines.
+cat $f | head -1
+echo
+
+echo "(8) DETECTIVES"
+# One more grep than the last.
+cat $f | head -1
+echo
+
+echo "(9) THE MODAL FIREMAN"
+# grep out the fire department, cut the salary column, sort it and count the number with each salary.
+# Then sort these and print the the most common occurrence.
+cat $f | head -1
+echo
+
+echo "(10) NAMES FOR POLICE OFFICERS"
+# sed to remove last names (preceding the first comma).
+# grep for police officers, and then remove everything else in the line.
+# sort the names, and use uniq to get their frequencies.
+cat $f | head -1
+echo
 
